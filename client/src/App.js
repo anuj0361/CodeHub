121f1f9b875b9755a1849f4e1475843d60d328db
unknown <anujpandey1630@gmail.com>
2019-01-06 20:51:57 +0530

    Adding Like & Comment post feature

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,8 @@ import AddEducation from "./components/add-details/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import "./App.css";
 
 if (localStorage.jwtToken) {
@@ -88,6 +90,12 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
               </Switch>
             </div>
             <Footer />
