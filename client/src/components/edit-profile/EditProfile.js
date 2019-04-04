e142a62f81a62a4db29d0a6387cb02281b6b8680
Anuj Pandey <anujpandey1630@gmail.com>
2019-04-04 16:07:11 +0530

    Update EditProfile.js

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -195,7 +195,7 @@ class CreateProfile extends Component {
               </Link>
               <h1 className="display-4 text-center">Edit Your Profile</h1>
               <p className="lead text-center">
-                Let's get some information to make your profile stand out
+                Let s get some information to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
